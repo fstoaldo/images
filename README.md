2f1a9b55458f535a402d0db2012411fda873371d
fstoaldo <fstoaldo@gmail.com>
2020-02-25 07:24:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Images
+
+### Initial goal:
+* get colour pallete of a given image (top 5 most frequent colors)
+
---
+++ src/images.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+
+# Owned
+__author__ = "fstoaldo"
+__maintainer__ = "fstoaldo"
+__email__ = "fstoaldo@gmail.com"
+
+"""images.py: the initial goal of this program is to open an image file and retrieve the top5 most frequent colours 
+of it. """
+
