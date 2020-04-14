c00e97cd36492ee7371dd4215e551adff500ff3b
fstoaldo <fstoaldo@gmail.com>
2020-04-14 12:00:28 -0300

    .

--- src/images.py
+++ src/images.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+from Pillow import Image test
 
 # Owned
 __author__ = "fstoaldo"
@@ -9,4 +10,19 @@ __email__ = "fstoaldo@gmail.com"
 
 """images.py: the initial goal of this program is to open an image file and retrieve the top5 most frequent colours 
 of it. """
+
+
+def main():
+    im = Image.open('dead_parrot.jpg')  # Can be many different formats.
+    pix = im.load()
+    print
+    im.size  # Get the width and hight of the image for iterating over
+    print
+    pix[x, y]  # Get the RGBA Value of the a pixel of an image
+    pix[x, y] = value  # Set the RGBA Value of the image (tuple)
+    im.save('alive_parrot.png')  # Save the modified pixels as .png
+
+
+if __name__ == '__main__':
+    main()
 
